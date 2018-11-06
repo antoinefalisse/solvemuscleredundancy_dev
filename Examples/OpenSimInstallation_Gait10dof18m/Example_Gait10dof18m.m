bd162d7d6abe93f3805931f673aa84ce7338b897
Antoine Falisse <antoinefalisse@gmail.com>
2018-11-06 14:37:33 +0100

    run all cases and save results

--- Examples/OpenSimInstallation_Gait10dof18m/Example_Gait10dof18m.m
+++ Examples/OpenSimInstallation_Gait10dof18m/Example_Gait10dof18m.m
@@ -46,36 +46,27 @@ Misc.Atendon=[35,35,35,35,35,35,15,15,35];
 
 %% Solve the problem
 [Time_l_G,MExcitation_l_G,MActivation_l_G,RActivation_l_G,TForcetilde_l_G,TForce_l_G,lMtilde_l_G,lM_l_G,MuscleNames_l_G,OptInfo_l_G,DatStore_l_G]=SolveMuscleRedundancy_lMtildeState_GPOPS(model_path,IK_path,ID_path,time,Out_path,Misc);
-[Time_l_C,MExcitation_l_C,MActivation_l_C,RActivation_l_C,TForcetilde_l_C,TForce_l_C,lMtilde_l_C,lM_l_C,MuscleNames_l_C,OptInfo_l_C,DatStore_l_C]=SolveMuscleRedundancy_lMtildeState_CasADi(model_path,IK_path,ID_path,time,Out_path,Misc);
-[Time_f_G,MExcitation_f_G,MActivation_f_G,RActivation_f_G,TForcetilde_f_G,TForce_f_G,lMtilde_f_G,lM_f_G,MuscleNames_f_G,OptInfo_f_G,DatStore_f_G]=SolveMuscleRedundancy_FtildeState_GPOPS(model_path,IK_path,ID_path,time,Out_path,Misc);
-[Time_f_C,MExcitation_f_C,MActivation_f_C,RActivation_f_C,TForcetilde_f_C,TForce_f_C,lMtilde_f_C,lM_f_C,MuscleNames_f_C,OptInfo_f_C,DatStore_f_C]=SolveMuscleRedundancy_FtildeState_CasADi(model_path,IK_path,ID_path,time,Out_path,Misc);
-
-[Time_l_actdyn_G,MExcitation_l_actdyn_G,MActivation_l_actdyn_G,RActivation_l_actdyn_G,TForcetilde_l_actdyn_G,TForce_l_actdyn_G,lMtilde_l_actdyn_G,lM_l_actdyn_G,MuscleNames_l_actdyn_G,OptInfo_l_actdyn_G,DatStore_l_actdyn_G]=SolveMuscleRedundancy_lMtildeState_actdyn_GPOPS(model_path,IK_path,ID_path,time,Out_path,Misc);
-[Time_l_actdyn_C,MExcitation_l_actdyn_C,MActivation_l_actdyn_C,RActivation_l_actdyn_C,TForcetilde_l_actdyn_C,TForce_l_actdyn_C,lMtilde_l_actdyn_C,lM_l_actdyn_C,MuscleNames_l_actdyn_C,OptInfo_l_actdyn_C,DatStore_l_actdyn_C]=SolveMuscleRedundancy_lMtildeState_actdyn_CasADi(model_path,IK_path,ID_path,time,Out_path,Misc);
-[Time_f_actdyn_G,MExcitation_f_actdyn_G,MActivation_f_actdyn_G,RActivation_f_actdyn_G,TForcetilde_f_actdyn_G,TForce_f_actdyn_G,lMtilde_f_actdyn_G,lM_f_actdyn_G,MuscleNames_f_actdyn_G,OptInfo_f_actdyn_G,DatStore_f_actdyn_G]=SolveMuscleRedundancy_FtildeState_actdyn_GPOPS(model_path,IK_path,ID_path,time,Out_path,Misc);
-[Time_f_actdyn_C,MExcitation_f_actdyn_C,MActivation_f_actdyn_C,RActivation_f_actdyn_C,TForcetilde_f_actdyn_C,TForce_f_actdyn_C,lMtilde_f_actdyn_C,lM_f_actdyn_C,MuscleNames_f_actdyn_C,OptInfo_f_actdyn_C,DatStore_f_actdyn_C]=SolveMuscleRedundancy_FtildeState_actdyn_CasADi(model_path,IK_path,ID_path,time,Out_path,Misc);
-
 save('Time_l_G','Time_l_G'); save('MExcitation_l_G','MExcitation_l_G'); save('MActivation_l_G','MActivation_l_G'); save('RActivation_l_G','RActivation_l_G');
 save('TForcetilde_l_G','TForcetilde_l_G'); save('TForce_l_G','TForce_l_G'); save('lMtilde_l_G','lMtilde_l_G'); save('lM_l_G','lM_l_G');
-
+[Time_l_C,MExcitation_l_C,MActivation_l_C,RActivation_l_C,TForcetilde_l_C,TForce_l_C,lMtilde_l_C,lM_l_C,MuscleNames_l_C,OptInfo_l_C,DatStore_l_C]=SolveMuscleRedundancy_lMtildeState_CasADi(model_path,IK_path,ID_path,time,Out_path,Misc);
 save('Time_l_C','Time_l_C'); save('MExcitation_l_C','MExcitation_l_C'); save('MActivation_l_C','MActivation_l_C'); save('RActivation_l_C','RActivation_l_C');
 save('TForcetilde_l_C','TForcetilde_l_C'); save('TForce_l_C','TForce_l_C'); save('lMtilde_l_C','lMtilde_l_C'); save('lM_l_C','lM_l_C');
-
+[Time_f_G,MExcitation_f_G,MActivation_f_G,RActivation_f_G,TForcetilde_f_G,TForce_f_G,lMtilde_f_G,lM_f_G,MuscleNames_f_G,OptInfo_f_G,DatStore_f_G]=SolveMuscleRedundancy_FtildeState_GPOPS(model_path,IK_path,ID_path,time,Out_path,Misc);
 save('Time_f_G','Time_f_G'); save('MExcitation_f_G','MExcitation_f_G'); save('MActivation_f_G','MActivation_f_G'); save('RActivation_f_G','RActivation_f_G');
 save('TForcetilde_f_G','TForcetilde_f_G'); save('TForce_f_G','TForce_f_G'); save('lMtilde_f_G','lMtilde_f_G'); save('lM_f_G','lM_f_G');
-
+[Time_f_C,MExcitation_f_C,MActivation_f_C,RActivation_f_C,TForcetilde_f_C,TForce_f_C,lMtilde_f_C,lM_f_C,MuscleNames_f_C,OptInfo_f_C,DatStore_f_C]=SolveMuscleRedundancy_FtildeState_CasADi(model_path,IK_path,ID_path,time,Out_path,Misc);
 save('Time_f_C','Time_f_C'); save('MExcitation_f_C','MExcitation_f_C'); save('MActivation_f_C','MActivation_f_C'); save('RActivation_f_C','RActivation_f_C');
 save('TForcetilde_f_C','TForcetilde_f_C'); save('TForce_f_C','TForce_f_C'); save('lMtilde_f_C','lMtilde_f_C'); save('lM_f_C','lM_f_C');
-
+[Time_l_actdyn_G,MExcitation_l_actdyn_G,MActivation_l_actdyn_G,RActivation_l_actdyn_G,TForcetilde_l_actdyn_G,TForce_l_actdyn_G,lMtilde_l_actdyn_G,lM_l_actdyn_G,MuscleNames_l_actdyn_G,OptInfo_l_actdyn_G,DatStore_l_actdyn_G]=SolveMuscleRedundancy_lMtildeState_actdyn_GPOPS(model_path,IK_path,ID_path,time,Out_path,Misc);
 save('Time_l_actdyn_G','Time_l_actdyn_G'); save('MExcitation_l_actdyn_G','MExcitation_l_actdyn_G'); save('MActivation_l_actdyn_G','MActivation_l_actdyn_G'); save('RActivation_l_actdyn_G','RActivation_l_actdyn_G');
 save('TForcetilde_l_actdyn_G','TForcetilde_l_actdyn_G'); save('TForce_l_actdyn_G','TForce_l_actdyn_G'); save('lMtilde_l_actdyn_G','lMtilde_l_actdyn_G'); save('lM_l_actdyn_G','lM_l_actdyn_G');
-
+[Time_l_actdyn_C,MExcitation_l_actdyn_C,MActivation_l_actdyn_C,RActivation_l_actdyn_C,TForcetilde_l_actdyn_C,TForce_l_actdyn_C,lMtilde_l_actdyn_C,lM_l_actdyn_C,MuscleNames_l_actdyn_C,OptInfo_l_actdyn_C,DatStore_l_actdyn_C]=SolveMuscleRedundancy_lMtildeState_actdyn_CasADi(model_path,IK_path,ID_path,time,Out_path,Misc);
 save('Time_l_actdyn_C','Time_l_actdyn_C'); save('MExcitation_l_actdyn_C','MExcitation_l_actdyn_C'); save('MActivation_l_actdyn_C','MActivation_l_actdyn_C'); save('RActivation_l_actdyn_C','RActivation_l_actdyn_C');
 save('TForcetilde_l_actdyn_C','TForcetilde_l_actdyn_C'); save('TForce_l_actdyn_C','TForce_l_actdyn_C'); save('lMtilde_l_actdyn_C','lMtilde_l_actdyn_C'); save('lM_l_actdyn_C','lM_l_actdyn_C');
-
+[Time_f_actdyn_G,MExcitation_f_actdyn_G,MActivation_f_actdyn_G,RActivation_f_actdyn_G,TForcetilde_f_actdyn_G,TForce_f_actdyn_G,lMtilde_f_actdyn_G,lM_f_actdyn_G,MuscleNames_f_actdyn_G,OptInfo_f_actdyn_G,DatStore_f_actdyn_G]=SolveMuscleRedundancy_FtildeState_actdyn_GPOPS(model_path,IK_path,ID_path,time,Out_path,Misc);
 save('Time_f_actdyn_G','Time_f_actdyn_G'); save('MExcitation_f_actdyn_G','MExcitation_f_actdyn_G'); save('MActivation_f_actdyn_G','MActivation_f_actdyn_G'); save('RActivation_f_actdyn_G','RActivation_f_actdyn_G');
 save('TForcetilde_f_actdyn_G','TForcetilde_f_actdyn_G'); save('TForce_f_actdyn_G','TForce_f_actdyn_G'); save('lMtilde_f_actdyn_G','lMtilde_f_actdyn_G'); save('lM_f_actdyn_G','lM_f_actdyn_G');
-
+[Time_f_actdyn_C,MExcitation_f_actdyn_C,MActivation_f_actdyn_C,RActivation_f_actdyn_C,TForcetilde_f_actdyn_C,TForce_f_actdyn_C,lMtilde_f_actdyn_C,lM_f_actdyn_C,MuscleNames_f_actdyn_C,OptInfo_f_actdyn_C,DatStore_f_actdyn_C]=SolveMuscleRedundancy_FtildeState_actdyn_CasADi(model_path,IK_path,ID_path,time,Out_path,Misc);
 save('Time_f_actdyn_C','Time_f_actdyn_C'); save('MExcitation_f_actdyn_C','MExcitation_f_actdyn_C'); save('MActivation_f_actdyn_C','MActivation_f_actdyn_C'); save('RActivation_f_actdyn_C','RActivation_f_actdyn_C');
 save('TForcetilde_f_actdyn_C','TForcetilde_f_actdyn_C'); save('TForce_f_actdyn_C','TForce_f_actdyn_C'); save('lMtilde_f_actdyn_C','lMtilde_f_actdyn_C'); save('lM_f_actdyn_C','lM_f_actdyn_C');
 
@@ -160,7 +151,7 @@ save('TForcetilde_f_actdyn_C','TForcetilde_f_actdyn_C'); save('TForce_f_actdyn_C
 % plot(Time_f_actdyn_G,MActivation_f_actdyn_G,'LineWidth',2);
 % plot(Time_l_C.collocationPoints,MActivation_l_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_C.collocationPoints,MActivation_f_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_l_actdyn_C.collocationPoints,MActivation_f_actdyn_G.collocationPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_actdyn_C.collocationPoints,MActivation_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_actdyn_C.collocationPoints,MActivation_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % title('Muscle activations')
 % 
@@ -171,10 +162,10 @@ save('TForcetilde_f_actdyn_C','TForcetilde_f_actdyn_C'); save('TForce_f_actdyn_C
 % plot(Time_f_G,MExcitation_f_G,'LineWidth',2);
 % plot(Time_l_actdyn_G,MExcitation_l_actdyn_G,'LineWidth',2);
 % plot(Time_f_actdyn_G,MExcitation_f_actdyn_G,'LineWidth',2);
-% plot(Time_l_C.meshPoints(1:end-1),MExcitation_l_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_f_C.meshPoints(1:end-1),MExcitation_f_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_l_actdyn_C.meshPoints(1:end-1),MExcitation_f_actdyn_G.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_f_actdyn_C.meshPoints(1:end-1),MExcitation_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_C.meshPoints(1:end-1),MExcitation_l_C.meshPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_f_C.meshPoints(1:end-1),MExcitation_f_C.meshPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_actdyn_C.meshPoints(1:end-1),MExcitation_f_actdyn_C.meshPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_f_actdyn_C.meshPoints(1:end-1),MExcitation_f_actdyn_C.meshPoints,'LineStyle',':','LineWidth',2);
 % title('Muscle excitations')
 % 
 % % Muscle tendon forces
@@ -186,7 +177,7 @@ save('TForcetilde_f_actdyn_C','TForcetilde_f_actdyn_C'); save('TForce_f_actdyn_C
 % plot(Time_f_actdyn_G,TForce_f_actdyn_G,'LineWidth',2);
 % plot(Time_l_C.collocationPoints,TForce_l_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_C.collocationPoints,TForce_f_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_l_actdyn_C.collocationPoints,TForce_f_actdyn_G.collocationPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_actdyn_C.collocationPoints,TForce_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_actdyn_C.collocationPoints,TForce_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % title('Muscle tendon forces')
 % 
@@ -199,9 +190,9 @@ save('TForcetilde_f_actdyn_C','TForcetilde_f_actdyn_C'); save('TForce_f_actdyn_C
 % plot(Time_f_actdyn_G,TForcetilde_f_actdyn_G,'LineWidth',2);
 % plot(Time_l_C.collocationPoints,TForcetilde_l_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_C.collocationPoints,TForcetilde_f_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_l_actdyn_C.collocationPoints,TForcetilde_f_actdyn_G.collocationPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_actdyn_C.collocationPoints,TForcetilde_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_actdyn_C.collocationPoints,TForcetilde_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% title('Normalized Muscle tendon forces')
+% title('Normalized muscle tendon forces')
 % 
 % % Muscle fiber lengths
 % figure()
@@ -212,7 +203,7 @@ save('TForcetilde_f_actdyn_C','TForcetilde_f_actdyn_C'); save('TForce_f_actdyn_C
 % plot(Time_f_actdyn_G,lM_f_actdyn_G,'LineWidth',2);
 % plot(Time_l_C.collocationPoints,lM_l_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_C.collocationPoints,lM_f_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_l_actdyn_C.collocationPoints,lM_f_actdyn_G.collocationPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_actdyn_C.collocationPoints,lM_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_actdyn_C.collocationPoints,lM_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % title('Muscle fiber lengths')
 % 
@@ -225,9 +216,9 @@ save('TForcetilde_f_actdyn_C','TForcetilde_f_actdyn_C'); save('TForce_f_actdyn_C
 % plot(Time_f_actdyn_G,lMtilde_f_actdyn_G,'LineWidth',2);
 % plot(Time_l_C.collocationPoints,lMtilde_l_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_C.collocationPoints,lMtilde_f_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_l_actdyn_C.collocationPoints,lMtilde_f_actdyn_G.collocationPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_actdyn_C.collocationPoints,lMtilde_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_actdyn_C.collocationPoints,lMtilde_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% title('Muscle fiber lengths')
+% title('Normalized muscle fiber lengths')
 % 
 % % Reserve actuators
 % figure()
@@ -236,8 +227,8 @@ save('TForcetilde_f_actdyn_C','TForcetilde_f_actdyn_C'); save('TForce_f_actdyn_C
 % plot(Time_f_G,RActivation_f_G,'LineWidth',2);
 % plot(Time_l_actdyn_G,RActivation_l_actdyn_G,'LineWidth',2);
 % plot(Time_f_actdyn_G,RActivation_f_actdyn_G,'LineWidth',2);
-% plot(Time_l_C.meshPoints(1:end-1),RActivation_l_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_f_C.meshPoints(1:end-1),RActivation_f_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_l_actdyn_C.meshPoints(1:end-1),RActivation_f_actdyn_G.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_f_actdyn_C.meshPoints(1:end-1),RActivation_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_C.meshPoints(1:end-1),RActivation_l_C.meshPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_f_C.meshPoints(1:end-1),RActivation_f_C.meshPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_actdyn_C.meshPoints(1:end-1),RActivation_f_actdyn_C.meshPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_f_actdyn_C.meshPoints(1:end-1),RActivation_f_actdyn_C.meshPoints,'LineStyle',':','LineWidth',2);
 % title('Reserve actuators')
--- Examples/OpenSimInstallation_Gait23dof54m/Example_gait23dof54m.m
+++ Examples/OpenSimInstallation_Gait23dof54m/Example_gait23dof54m.m
@@ -33,36 +33,27 @@ Misc.MuscleNames_Input={}; % Selects all muscles for input DOFs when empty
 
 %% Solve the problem
 [Time_l_G,MExcitation_l_G,MActivation_l_G,RActivation_l_G,TForcetilde_l_G,TForce_l_G,lMtilde_l_G,lM_l_G,MuscleNames_l_G,OptInfo_l_G,DatStore_l_G]=SolveMuscleRedundancy_lMtildeState_GPOPS(model_path,IK_path,ID_path,time,Out_path,Misc);
-[Time_l_C,MExcitation_l_C,MActivation_l_C,RActivation_l_C,TForcetilde_l_C,TForce_l_C,lMtilde_l_C,lM_l_C,MuscleNames_l_C,OptInfo_l_C,DatStore_l_C]=SolveMuscleRedundancy_lMtildeState_CasADi(model_path,IK_path,ID_path,time,Out_path,Misc);
-[Time_f_G,MExcitation_f_G,MActivation_f_G,RActivation_f_G,TForcetilde_f_G,TForce_f_G,lMtilde_f_G,lM_f_G,MuscleNames_f_G,OptInfo_f_G,DatStore_f_G]=SolveMuscleRedundancy_FtildeState_GPOPS(model_path,IK_path,ID_path,time,Out_path,Misc);
-[Time_f_C,MExcitation_f_C,MActivation_f_C,RActivation_f_C,TForcetilde_f_C,TForce_f_C,lMtilde_f_C,lM_f_C,MuscleNames_f_C,OptInfo_f_C,DatStore_f_C]=SolveMuscleRedundancy_FtildeState_CasADi(model_path,IK_path,ID_path,time,Out_path,Misc);
-
-[Time_l_actdyn_G,MExcitation_l_actdyn_G,MActivation_l_actdyn_G,RActivation_l_actdyn_G,TForcetilde_l_actdyn_G,TForce_l_actdyn_G,lMtilde_l_actdyn_G,lM_l_actdyn_G,MuscleNames_l_actdyn_G,OptInfo_l_actdyn_G,DatStore_l_actdyn_G]=SolveMuscleRedundancy_lMtildeState_actdyn_GPOPS(model_path,IK_path,ID_path,time,Out_path,Misc);
-[Time_l_actdyn_C,MExcitation_l_actdyn_C,MActivation_l_actdyn_C,RActivation_l_actdyn_C,TForcetilde_l_actdyn_C,TForce_l_actdyn_C,lMtilde_l_actdyn_C,lM_l_actdyn_C,MuscleNames_l_actdyn_C,OptInfo_l_actdyn_C,DatStore_l_actdyn_C]=SolveMuscleRedundancy_lMtildeState_actdyn_CasADi(model_path,IK_path,ID_path,time,Out_path,Misc);
-[Time_f_actdyn_G,MExcitation_f_actdyn_G,MActivation_f_actdyn_G,RActivation_f_actdyn_G,TForcetilde_f_actdyn_G,TForce_f_actdyn_G,lMtilde_f_actdyn_G,lM_f_actdyn_G,MuscleNames_f_actdyn_G,OptInfo_f_actdyn_G,DatStore_f_actdyn_G]=SolveMuscleRedundancy_FtildeState_actdyn_GPOPS(model_path,IK_path,ID_path,time,Out_path,Misc);
-[Time_f_actdyn_C,MExcitation_f_actdyn_C,MActivation_f_actdyn_C,RActivation_f_actdyn_C,TForcetilde_f_actdyn_C,TForce_f_actdyn_C,lMtilde_f_actdyn_C,lM_f_actdyn_C,MuscleNames_f_actdyn_C,OptInfo_f_actdyn_C,DatStore_f_actdyn_C]=SolveMuscleRedundancy_FtildeState_actdyn_CasADi(model_path,IK_path,ID_path,time,Out_path,Misc);
-
 save('Time_l_G','Time_l_G'); save('MExcitation_l_G','MExcitation_l_G'); save('MActivation_l_G','MActivation_l_G'); save('RActivation_l_G','RActivation_l_G');
 save('TForcetilde_l_G','TForcetilde_l_G'); save('TForce_l_G','TForce_l_G'); save('lMtilde_l_G','lMtilde_l_G'); save('lM_l_G','lM_l_G');
-
+[Time_l_C,MExcitation_l_C,MActivation_l_C,RActivation_l_C,TForcetilde_l_C,TForce_l_C,lMtilde_l_C,lM_l_C,MuscleNames_l_C,OptInfo_l_C,DatStore_l_C]=SolveMuscleRedundancy_lMtildeState_CasADi(model_path,IK_path,ID_path,time,Out_path,Misc);
 save('Time_l_C','Time_l_C'); save('MExcitation_l_C','MExcitation_l_C'); save('MActivation_l_C','MActivation_l_C'); save('RActivation_l_C','RActivation_l_C');
 save('TForcetilde_l_C','TForcetilde_l_C'); save('TForce_l_C','TForce_l_C'); save('lMtilde_l_C','lMtilde_l_C'); save('lM_l_C','lM_l_C');
-
+[Time_f_G,MExcitation_f_G,MActivation_f_G,RActivation_f_G,TForcetilde_f_G,TForce_f_G,lMtilde_f_G,lM_f_G,MuscleNames_f_G,OptInfo_f_G,DatStore_f_G]=SolveMuscleRedundancy_FtildeState_GPOPS(model_path,IK_path,ID_path,time,Out_path,Misc);
 save('Time_f_G','Time_f_G'); save('MExcitation_f_G','MExcitation_f_G'); save('MActivation_f_G','MActivation_f_G'); save('RActivation_f_G','RActivation_f_G');
 save('TForcetilde_f_G','TForcetilde_f_G'); save('TForce_f_G','TForce_f_G'); save('lMtilde_f_G','lMtilde_f_G'); save('lM_f_G','lM_f_G');
-
+[Time_f_C,MExcitation_f_C,MActivation_f_C,RActivation_f_C,TForcetilde_f_C,TForce_f_C,lMtilde_f_C,lM_f_C,MuscleNames_f_C,OptInfo_f_C,DatStore_f_C]=SolveMuscleRedundancy_FtildeState_CasADi(model_path,IK_path,ID_path,time,Out_path,Misc);
 save('Time_f_C','Time_f_C'); save('MExcitation_f_C','MExcitation_f_C'); save('MActivation_f_C','MActivation_f_C'); save('RActivation_f_C','RActivation_f_C');
 save('TForcetilde_f_C','TForcetilde_f_C'); save('TForce_f_C','TForce_f_C'); save('lMtilde_f_C','lMtilde_f_C'); save('lM_f_C','lM_f_C');
-
+[Time_l_actdyn_G,MExcitation_l_actdyn_G,MActivation_l_actdyn_G,RActivation_l_actdyn_G,TForcetilde_l_actdyn_G,TForce_l_actdyn_G,lMtilde_l_actdyn_G,lM_l_actdyn_G,MuscleNames_l_actdyn_G,OptInfo_l_actdyn_G,DatStore_l_actdyn_G]=SolveMuscleRedundancy_lMtildeState_actdyn_GPOPS(model_path,IK_path,ID_path,time,Out_path,Misc);
 save('Time_l_actdyn_G','Time_l_actdyn_G'); save('MExcitation_l_actdyn_G','MExcitation_l_actdyn_G'); save('MActivation_l_actdyn_G','MActivation_l_actdyn_G'); save('RActivation_l_actdyn_G','RActivation_l_actdyn_G');
 save('TForcetilde_l_actdyn_G','TForcetilde_l_actdyn_G'); save('TForce_l_actdyn_G','TForce_l_actdyn_G'); save('lMtilde_l_actdyn_G','lMtilde_l_actdyn_G'); save('lM_l_actdyn_G','lM_l_actdyn_G');
-
+[Time_l_actdyn_C,MExcitation_l_actdyn_C,MActivation_l_actdyn_C,RActivation_l_actdyn_C,TForcetilde_l_actdyn_C,TForce_l_actdyn_C,lMtilde_l_actdyn_C,lM_l_actdyn_C,MuscleNames_l_actdyn_C,OptInfo_l_actdyn_C,DatStore_l_actdyn_C]=SolveMuscleRedundancy_lMtildeState_actdyn_CasADi(model_path,IK_path,ID_path,time,Out_path,Misc);
 save('Time_l_actdyn_C','Time_l_actdyn_C'); save('MExcitation_l_actdyn_C','MExcitation_l_actdyn_C'); save('MActivation_l_actdyn_C','MActivation_l_actdyn_C'); save('RActivation_l_actdyn_C','RActivation_l_actdyn_C');
 save('TForcetilde_l_actdyn_C','TForcetilde_l_actdyn_C'); save('TForce_l_actdyn_C','TForce_l_actdyn_C'); save('lMtilde_l_actdyn_C','lMtilde_l_actdyn_C'); save('lM_l_actdyn_C','lM_l_actdyn_C');
-
+[Time_f_actdyn_G,MExcitation_f_actdyn_G,MActivation_f_actdyn_G,RActivation_f_actdyn_G,TForcetilde_f_actdyn_G,TForce_f_actdyn_G,lMtilde_f_actdyn_G,lM_f_actdyn_G,MuscleNames_f_actdyn_G,OptInfo_f_actdyn_G,DatStore_f_actdyn_G]=SolveMuscleRedundancy_FtildeState_actdyn_GPOPS(model_path,IK_path,ID_path,time,Out_path,Misc);
 save('Time_f_actdyn_G','Time_f_actdyn_G'); save('MExcitation_f_actdyn_G','MExcitation_f_actdyn_G'); save('MActivation_f_actdyn_G','MActivation_f_actdyn_G'); save('RActivation_f_actdyn_G','RActivation_f_actdyn_G');
 save('TForcetilde_f_actdyn_G','TForcetilde_f_actdyn_G'); save('TForce_f_actdyn_G','TForce_f_actdyn_G'); save('lMtilde_f_actdyn_G','lMtilde_f_actdyn_G'); save('lM_f_actdyn_G','lM_f_actdyn_G');
-
+[Time_f_actdyn_C,MExcitation_f_actdyn_C,MActivation_f_actdyn_C,RActivation_f_actdyn_C,TForcetilde_f_actdyn_C,TForce_f_actdyn_C,lMtilde_f_actdyn_C,lM_f_actdyn_C,MuscleNames_f_actdyn_C,OptInfo_f_actdyn_C,DatStore_f_actdyn_C]=SolveMuscleRedundancy_FtildeState_actdyn_CasADi(model_path,IK_path,ID_path,time,Out_path,Misc);
 save('Time_f_actdyn_C','Time_f_actdyn_C'); save('MExcitation_f_actdyn_C','MExcitation_f_actdyn_C'); save('MActivation_f_actdyn_C','MActivation_f_actdyn_C'); save('RActivation_f_actdyn_C','RActivation_f_actdyn_C');
 save('TForcetilde_f_actdyn_C','TForcetilde_f_actdyn_C'); save('TForce_f_actdyn_C','TForce_f_actdyn_C'); save('lMtilde_f_actdyn_C','lMtilde_f_actdyn_C'); save('lM_f_actdyn_C','lM_f_actdyn_C');
 
@@ -147,7 +138,7 @@ save('TForcetilde_f_actdyn_C','TForcetilde_f_actdyn_C'); save('TForce_f_actdyn_C
 % plot(Time_f_actdyn_G,MActivation_f_actdyn_G,'LineWidth',2);
 % plot(Time_l_C.collocationPoints,MActivation_l_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_C.collocationPoints,MActivation_f_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_l_actdyn_C.collocationPoints,MActivation_f_actdyn_G.collocationPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_actdyn_C.collocationPoints,MActivation_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_actdyn_C.collocationPoints,MActivation_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % title('Muscle activations')
 % 
@@ -158,10 +149,10 @@ save('TForcetilde_f_actdyn_C','TForcetilde_f_actdyn_C'); save('TForce_f_actdyn_C
 % plot(Time_f_G,MExcitation_f_G,'LineWidth',2);
 % plot(Time_l_actdyn_G,MExcitation_l_actdyn_G,'LineWidth',2);
 % plot(Time_f_actdyn_G,MExcitation_f_actdyn_G,'LineWidth',2);
-% plot(Time_l_C.meshPoints(1:end-1),MExcitation_l_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_f_C.meshPoints(1:end-1),MExcitation_f_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_l_actdyn_C.meshPoints(1:end-1),MExcitation_f_actdyn_G.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_f_actdyn_C.meshPoints(1:end-1),MExcitation_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_C.meshPoints(1:end-1),MExcitation_l_C.meshPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_f_C.meshPoints(1:end-1),MExcitation_f_C.meshPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_actdyn_C.meshPoints(1:end-1),MExcitation_f_actdyn_C.meshPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_f_actdyn_C.meshPoints(1:end-1),MExcitation_f_actdyn_C.meshPoints,'LineStyle',':','LineWidth',2);
 % title('Muscle excitations')
 % 
 % % Muscle tendon forces
@@ -173,7 +164,7 @@ save('TForcetilde_f_actdyn_C','TForcetilde_f_actdyn_C'); save('TForce_f_actdyn_C
 % plot(Time_f_actdyn_G,TForce_f_actdyn_G,'LineWidth',2);
 % plot(Time_l_C.collocationPoints,TForce_l_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_C.collocationPoints,TForce_f_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_l_actdyn_C.collocationPoints,TForce_f_actdyn_G.collocationPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_actdyn_C.collocationPoints,TForce_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_actdyn_C.collocationPoints,TForce_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % title('Muscle tendon forces')
 % 
@@ -186,9 +177,9 @@ save('TForcetilde_f_actdyn_C','TForcetilde_f_actdyn_C'); save('TForce_f_actdyn_C
 % plot(Time_f_actdyn_G,TForcetilde_f_actdyn_G,'LineWidth',2);
 % plot(Time_l_C.collocationPoints,TForcetilde_l_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_C.collocationPoints,TForcetilde_f_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_l_actdyn_C.collocationPoints,TForcetilde_f_actdyn_G.collocationPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_actdyn_C.collocationPoints,TForcetilde_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_actdyn_C.collocationPoints,TForcetilde_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% title('Normalized Muscle tendon forces')
+% title('Normalized muscle tendon forces')
 % 
 % % Muscle fiber lengths
 % figure()
@@ -199,7 +190,7 @@ save('TForcetilde_f_actdyn_C','TForcetilde_f_actdyn_C'); save('TForce_f_actdyn_C
 % plot(Time_f_actdyn_G,lM_f_actdyn_G,'LineWidth',2);
 % plot(Time_l_C.collocationPoints,lM_l_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_C.collocationPoints,lM_f_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_l_actdyn_C.collocationPoints,lM_f_actdyn_G.collocationPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_actdyn_C.collocationPoints,lM_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_actdyn_C.collocationPoints,lM_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % title('Muscle fiber lengths')
 % 
@@ -212,9 +203,9 @@ save('TForcetilde_f_actdyn_C','TForcetilde_f_actdyn_C'); save('TForce_f_actdyn_C
 % plot(Time_f_actdyn_G,lMtilde_f_actdyn_G,'LineWidth',2);
 % plot(Time_l_C.collocationPoints,lMtilde_l_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_C.collocationPoints,lMtilde_f_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_l_actdyn_C.collocationPoints,lMtilde_f_actdyn_G.collocationPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_actdyn_C.collocationPoints,lMtilde_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
 % plot(Time_f_actdyn_C.collocationPoints,lMtilde_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% title('Muscle fiber lengths')
+% title('Normalized muscle fiber lengths')
 % 
 % % Reserve actuators
 % figure()
@@ -223,8 +214,8 @@ save('TForcetilde_f_actdyn_C','TForcetilde_f_actdyn_C'); save('TForce_f_actdyn_C
 % plot(Time_f_G,RActivation_f_G,'LineWidth',2);
 % plot(Time_l_actdyn_G,RActivation_l_actdyn_G,'LineWidth',2);
 % plot(Time_f_actdyn_G,RActivation_f_actdyn_G,'LineWidth',2);
-% plot(Time_l_C.meshPoints(1:end-1),RActivation_l_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_f_C.meshPoints(1:end-1),RActivation_f_C.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_l_actdyn_C.meshPoints(1:end-1),RActivation_f_actdyn_G.collocationPoints,'LineStyle',':','LineWidth',2);
-% plot(Time_f_actdyn_C.meshPoints(1:end-1),RActivation_f_actdyn_C.collocationPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_C.meshPoints(1:end-1),RActivation_l_C.meshPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_f_C.meshPoints(1:end-1),RActivation_f_C.meshPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_l_actdyn_C.meshPoints(1:end-1),RActivation_f_actdyn_C.meshPoints,'LineStyle',':','LineWidth',2);
+% plot(Time_f_actdyn_C.meshPoints(1:end-1),RActivation_f_actdyn_C.meshPoints,'LineStyle',':','LineWidth',2);
 % title('Reserve actuators')
